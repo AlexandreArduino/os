37b701a5949e4fb9afcfb15da4df777849687dcc
AlexandreArduino <alexandre10baalbaky@gmail.com>
2021-06-12 12:54:24 +0200

    Cursor class finished!

--- dev/cursor.cpp
+++ dev/cursor.cpp
@@ -8,7 +8,7 @@ Dev::Cursor::Cursor(int location)
 
 Dev::Cursor::Cursor(int x, int y)
 {
-
+    Dev::Cursor::SetLocation(x, y);
 }
 
 Dev::Cursor::~Cursor(){}
@@ -16,6 +16,8 @@ Dev::Cursor::~Cursor(){}
 void Dev::Cursor::SetLocation(int location)
 {
     Dev::Cursor::location = Dev::Cursor::VerifCoos(location);
+    Dev::Cursor::x = Dev::Cursor::GetX();
+    Dev::Cursor::y = Dev::Cursor::GetY();
     IO::outb(0x3D4, 0x0F);
     IO::outb(0x3D5, (unsigned char)(Dev::Cursor::location & 0xFF));
     IO::outb(0x3D4, 0x0E);
@@ -30,4 +32,44 @@ int Dev::Cursor::VerifCoos(int location)
         return SIZE_SCREEN - 1;
     else
         return location;
+}
+
+void Dev::Cursor::SetLocation(int x, int y)
+{
+    Dev::Cursor::SetLocation(Dev::Cursor::GetLocation(x, y));
+}
+
+int Dev::Cursor::GetLocation(int x, int y)
+{
+    return x + y*LENGTH_LINE;
+}
+
+int Dev::Cursor::GetX()
+{
+    return Dev::Cursor::location % LENGTH_LINE;
+}
+
+int Dev::Cursor::GetY()
+{
+    return (int)Dev::Cursor::location / LENGTH_LINE;
+}
+
+int Dev::Cursor::GetLocation()
+{
+    return Dev::Cursor::location;
+}
+
+void Dev::Cursor::AddValue(int value)
+{
+    Dev::Cursor::SetLocation(Dev::Cursor::location + value);
+}
+
+void Dev::Cursor::AddX(int x)
+{
+    Dev::Cursor::SetLocation(Dev::Cursor::GetX() + x, Dev::Cursor::GetY());
+}
+
+void Dev::Cursor::AddY(int y)
+{
+    Dev::Cursor::SetLocation(Dev::Cursor::GetX(), Dev::Cursor::GetY() + y);
 }
--- dev/cursor.h
+++ dev/cursor.h
@@ -10,7 +10,15 @@ namespace Dev
         Cursor(int x, int y);
         ~Cursor();
         void SetLocation(int location);
+        void SetLocation(int x, int y);
+        int GetLocation(int x, int y);
+        int GetLocation();
+        int GetX();
+        int GetY();
         int VerifCoos(int location);
+        void AddValue(int value);
+        void AddX(int x);
+        void AddY(int y);
     private:
         int location;
         int x;
