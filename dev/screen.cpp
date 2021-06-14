0bfa2d00e8c1120fa488ff10c7de46daad2863b4
AlexandreArduino <alexandre10baalbaky@gmail.com>
2021-06-14 11:45:42 +0200

    Working on scroll function!

--- dev/screen.cpp
+++ dev/screen.cpp
@@ -4,11 +4,83 @@
 /*
 * To access to cursor object : Dev::Screen::cursor->function;
 */
+
 Dev::Screen::Screen(int CursorLocation)
 {
     TextCursor _cursor(CursorLocation);
     Dev::Screen::cursor = &_cursor;
-    Dev::Screen::cursor->SetLocation(5);
+    Dev::Screen::print("Hello World!", YELLOW);
+    Dev::Screen::println("Hi, everyone!", GREEN);
+    Dev::Screen::print("Hello World!", 5, BLUE);
+    Dev::Screen::println("Test of function", 250, WHITE);
+    Dev::Screen::scroll(2);
 }
 
 Dev::Screen::~Screen(){}
+
+void Dev::Screen::putchar(char c, unsigned int position, u8 color)
+{
+    char *video = (char*)(VIDEO_MEMORY + position*2);
+    *video = c;
+    video++;
+    *video = color;
+    Dev::Screen::cursor->SetLocation(position + 1);
+}
+
+void Dev::Screen::putchar(char c, u8 color)
+{
+    Dev::Screen::putchar(c, cursor->GetLocation(), color);
+}
+
+void Dev::Screen::print(char *str, u8 color)
+{
+    while(*str != 0)
+    {
+        putchar(*str, color);
+        str++;
+    }
+}
+void Dev::Screen::println(char *str, u8 color)
+{
+    while(*str != 0)
+    {
+        putchar(*str, color);
+        str++;
+    }
+    cursor->AddY(1);
+    cursor->SetLocation(cursor->GetX() - cursor->GetX(), cursor->GetY());
+}
+
+void Dev::Screen::print(char *str, unsigned int position, u8 color)
+{
+    unsigned int count = 0;
+    while(*str != 0)
+    {
+        putchar(*str, position + count, color);
+        str++;
+        count++;
+    }
+}
+
+void Dev::Screen::println(char *str, unsigned int position, u8 color)
+{
+    Dev::Screen::print(str, position, color);
+    cursor->AddY(1);
+    cursor->SetLocation(cursor->GetX() - cursor->GetX(), cursor->GetY());
+}
+
+void Dev::Screen::scroll(u8 NumberLines)
+{
+    int last_y = cursor->GetY();
+    int last_x = cursor->GetX();
+    char *current;
+    char *destination;
+    int i;
+    for(i = 0; i < SIZE_SCREEN - NumberLines*LENGTH_LINE;i++)
+    {
+        current = (char*)(VIDEO_MEMORY + LENGTH_LINE*NumberLines*2 + i);
+        destination = (char*)(VIDEO_MEMORY + i);
+        *destination = *current;
+    }
+    cursor->SetLocation(last_x, last_y - NumberLines);
+}
--- dev/screen.h
+++ dev/screen.h
@@ -8,6 +8,13 @@ namespace Dev
     public:
         Screen(int CursorLocation);
         ~Screen();
+        void putchar(char c, unsigned int position, u8 color);
+        void putchar(char c, u8 color);
+        void print(char *str, u8 color);
+        void println(char *str, u8 color);
+        void print(char *str, unsigned int position, u8 color);
+        void println(char *str, unsigned int position, u8 color);
+        void scroll(u8 NumberLines);
         TextCursor *cursor;
     };
 };
